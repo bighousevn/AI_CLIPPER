ee03cda26ceb95225976d93fac20b44f2b5cbfd4
Ductran-05 <23520320@gm.uit.edu.vn>
2025-10-03 17:41:04 +0700

    call api login, sign up

--- FE/src/server/auth/config.ts
+++ FE/src/server/auth/config.ts
@@ -1,8 +1,5 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
-import { comparePasswords } from "~/lib/auth";
-import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -25,44 +22,10 @@ declare module "next-auth" {
   // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authConfig = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null;
-        }
-
-        const email = credentials.email as string;
-        const password = credentials.password as string;
-
-        const user = await db.user.findUnique({
-          where: { email },
-        });
-
-        if (!user) {
-          return null;
-        }
-
-        const passwordMatch = await comparePasswords(password, user.password);
-        if (!passwordMatch) return null;
-
-        return user;
-      },
-    }),
-  ],
+  // Không cần credentials provider nữa
+  providers: [],
   session: { strategy: "jwt" },
-  adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, token }) => ({
       ...session,
