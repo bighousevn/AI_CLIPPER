cdc8c173ab6a97b8f9c57ec7410af85452be0dc3
Ductran-05 <23520320@gm.uit.edu.vn>
2025-10-04 00:30:11 +0700

    dark-mode toggle

--- FE/src/app/dashboard/layout.tsx
+++ FE/src/app/dashboard/layout.tsx
@@ -1,24 +1,24 @@
 "use server";
 
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Toaster } from "sonner";
 import NavHeader from "~/components/ui/nav-header";
-import { auth } from "~/server/auth";
+
+interface User {
+    email: string;
+    credits: number;
+}
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
 
-    // const user = await db.user.findUniqueOrThrow({
-    //     where: {
-    //         id: session.user.id,
-    //     },
-    //     select: {
-    //         email: true,
-    //         credits: true,
-    //     },
-    // });
+
+    let user: User = { email: "Duc", credits: 10 };
+
+
     return (
         <div className="flex min-h-screen flex-col ">
-            {/* <NavHeader credits={user.credits} email={user.email} /> */}
+            <NavHeader credits={user.credits} email={user.email} />
             <main className="flex-1 container py-6 mx-auto ">
                 {children}
             </main>
